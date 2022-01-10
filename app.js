bc384c637a2e08b9694aebe16ad1e5492f209f7e
Pooja Kelkar <poojakelkar53@gmail.com>
2022-01-10 20:18:14 +0530

    completed 3 assigmnent

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-var aloo = 1;
-if (aloo == 1) {
-   var a = 2;
-   console.log(a);
+//Assignment 1:
+// var aloo = 1;
+// if (aloo == 1) {
+//    var a = 2;
+//    console.log(a);
+// }
+// console.log(aloo);
+if(aloo=1){ 
+    console.log(a=2) 
+    console.log(aloo=1)
 }
-console.log(aloo);
+//Assignment 2:
+// var multiply = function(x, y) {
+//     return x * y;
+//   };
+var multiply = (x,y)=> x*y
+console.log(multiply(2,4))
+
+//Assignment 3:
+// var customer = {
+//     name: "Bhaalo"
+//   };
+//   var card = {
+//     amount: 20,
+//     product: "Aaalo",
+//     unitprice: 50
+//   };
+//   var message = "Hello " + customer.name + " wants to buy " + card.amount + " " + card.product + " for price of " + card.unitprice + " per piece"
+
+var customer = { name: "bhaloo" };
+var card = { amount: 20, product: "aaloo", unitprice: 50 };
+var message = `hello ${customer.name} want to buy ${card.amount} ${card.product} for price of ${card.unitprice} per price`;
+console.log(message)
+
+var Neog = ["Tanvi", "Swap", "Tanay", "MA", "CA", "PA", "TA"],
+CEO = Neog[0],
+Mentor = Neog[2];
+
+
