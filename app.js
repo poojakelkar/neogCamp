3933d685e8ef3c1ca2171bcbcef8a273b6fcfd32
Pooja Kelkar <poojakelkar53@gmail.com>
2022-01-11 21:19:40 +0530

    Assignment 3 4 5 6 completed

--- app.js
+++ app.js
@@ -32,8 +32,39 @@ var card = { amount: 20, product: "aaloo", unitprice: 50 };
 var message = `hello ${customer.name} want to buy ${card.amount} ${card.product} for price of ${card.unitprice} per price`;
 console.log(message)
 
+//assignment 4:
+// var Neog = ["Tanvi", "Swap", "Tanay", "MA", "CA", "PA", "TA"],
+// CEO = Neog[0],
+// Mentor = Neog[2];
+// console.log(Neog)
+
 var Neog = ["Tanvi", "Swap", "Tanay", "MA", "CA", "PA", "TA"],
-CEO = Neog[0],
-Mentor = Neog[2];
+[CEO , Mentor]=[Neog[0],Neog[2]]
+console.log(CEO)
+
+// Assignment:5
+// var arr = ["MA", "TA", "PA", "CA"];
+// var firstName = arr[0],
+// var surname = arr[1];
+// console.log(firstName);
+// console.log(surname);
+var arr = ["MA", "TA", "PA", "CA"];
+[firstname,surname]=[arr[0],arr[1]]
+console.log(firstname)
+
+
+// Assignment:6
+// var Aaloo = "Tasty";
+// var Bhaloo = "Cute";
+// var Obj = {
+//   Aaloo: Aaloo,
+//   Bhaloo: Bhaloo
+// };
+
+var obj = {Aaloo:"Tasty",bhaloo:"cute"}
+console.log(obj)
+
+Assignment 7:
+
 
 
